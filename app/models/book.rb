4a24289a085bac71e4e19107353996d172c6802f
Giovanna Montemurro <giovannasm@gmail.com>
2022-08-31 15:19:50 -0300

    todas as rotas básicas funcionando

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :trades, dependent: :destroy, foreign_key: 'seller_book_id'
+  has_many :trades, dependent: :destroy, foreign_key: 'buyer_book_id'
 
   validates :description, presence: true
 
