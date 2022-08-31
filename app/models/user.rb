376b603452401cf2e836127ac6c5c65b2a762652
Giovanna Montemurro <giovannasm@gmail.com>
2022-08-31 10:46:40 -0300

    validações de user arrumadas

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,4 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :username, :email, :firstname, :lastname, :address, presence: true
-  validates :username, uniqueness: true
 end
