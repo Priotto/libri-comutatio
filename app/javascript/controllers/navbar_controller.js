8af38391560a9bf7161f15cd0599078b6b370dbd
Hugo Prioto <hugo.prioto@gmail.com>
2022-09-05 11:04:13 -0300

    Implementação do Geocoding - Solved

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+
   }
   updateNavbar() {
     if (window.scrollY >= window.innerHeight) {
@@ -11,5 +12,3 @@ export default class extends Controller {
     }
   }
 }
-
-
