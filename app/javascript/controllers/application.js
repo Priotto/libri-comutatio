09e252434b612d5fce9dbb529ae2b660121cdd2b
cyd losekann <cydnel@gmail.com>
2022-09-06 16:36:17 -0300

    botao para o topo da pg

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,7 @@
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 import { Autocomplete } from 'stimulus-autocomplete'
-
+import ScrollTo from 'stimulus-scroll-to'
 
 const application = Application.start()
 
@@ -9,6 +9,7 @@ const application = Application.start()
 application.debug = false
 const context = require.context("./", true, /\.js$/);
 application.register('autocomplete', Autocomplete)
+application.register('scroll-to', ScrollTo)
 application.load(definitionsFromContext(context));
 window.Stimulus   = application
 
